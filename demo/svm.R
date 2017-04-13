8410b78304635c33aee84dc586bc62d8f03f49bf
anqif <anqif@stanford.edu>
2017-04-13 14:47:24 -0700

    WIP support vector classifier example

--- demo/svm.R
+++ demo/svm.R
@@ -1,4 +1,5 @@
 # Binary support vector classifier
+set.seed(10)
 n <- 2
 m <- 50
 
@@ -9,19 +10,20 @@ X[y == 1,] = X[y == 1,] + 1
 
 # Form problem
 cost <- 10
+beta0 <- Variable()
 beta <- Variable(n)
 slack <- Variable(m)
-loss <- (1/2)*sum(beta) + cost*sum(slack)
-constr <- list(y * (X %*% beta) >= 1 - slack, slack >= 0)
+loss <- (1/2)*SumSquares(VStack(beta, beta0)) + cost*sum(slack)
+constr <- list(y * (X %*% beta + beta0) >= 1 - slack, slack >= 0)
 prob <- Problem(Minimize(loss), constr)
 
 # Solve and plot result
 result <- solve(prob)
 result$optimal_value
 # TODO: More user-friendly functions to retrieve results
+b0 <- result$primal_values[[as.character(beta0@id)]]
 bres <- result$primal_values[[as.character(beta@id)]]
-sres <- result$primal_values[[as.character(slack@id)]]
 
 plot(X, col = (3-y), main = "Support Vector Classifier")
 legend("topleft", paste("y =", unique(y)), col = 3-unique(y), lty = 1)
-abline(a = 1/bres[2], b = -bres[1]/bres[2])
+abline(a = (1-b0)/bres[2], b = -bres[1]/bres[2])

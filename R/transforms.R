7be67c1b80fd126e309796649dfb63fef135b582
anqif <anqif@stanford.edu>
2017-09-05 14:01:16 -0700

    WIP Adding transformation functions and objects

---
+++ R/transforms.R
@@ -0,0 +1,23 @@
+linearize <- function(expr) {
+  expr <- as.Constant(expr)
+  if(is_affine(expr))
+    return(expr)
+  else {
+    tangent <- value(expr)
+    if(is.na(tangent))
+      stop("Cannot linearize non-affine expression with missing variable values.")
+    grad_map <- grad(expr)
+    for(var in variables(expr)) {
+      grad_var <- grad_map[[as.character(var@id)]]
+      if(is.na(grad_var))
+        return(NA_real_)
+      else if(is_matrix(var)) {
+        flattened <- t(Constant(grad_var)) %*% Vec(var - value(var))
+        size <- size(expr)
+        tangent <- tangent + Reshape(flattened, size[1], size[2])
+      } else
+        tangent <- tangent + t(Constant(grad_var)) %*% (var - value(var))
+    }
+  }
+  return(tangent)
+}

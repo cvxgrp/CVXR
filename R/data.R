a4b01c70e6114095de312bfa338746fe208e899a
Balasubramanian Narasimhan <b.naras@gmail.com>
2017-10-27 20:46:10 -0700

    Added global warming dataset
    Added global warming dataset

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Global Monthly and Annual Temperature Anomalies (degrees C), 1850-2015
+#' (Relative to the 1961-1990 Mean) (May 2016)
+#'
+#' @format A data frame with 166 rows and 14 variables
+#' \describe{
+#'   \item{YEAR}{year}
+#'   \item{JAN, FEB, ..., DEC}{Anomaly for each month in the respective year}
+#'   \item{ANNUAL}{Annual anomaly for the year}
+#' }
+#' @source \url{http://cdiac.ess-dive.lbl.gov/trends/temp/jonescru/data.html}

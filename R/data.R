89245bde51dd76abf854eb83abb43af9ddf9462d
Balasubramanian Narasimhan <b.naras@gmail.com>
2017-11-02 15:01:19 -0700

    Documented global warming data
    Documented global warming data and made everything lower case to match
    paper

--- R/data.R
+++ R/data.R
@@ -1,10 +1,15 @@
 #' Global Monthly and Annual Temperature Anomalies (degrees C), 1850-2015
 #' (Relative to the 1961-1990 Mean) (May 2016)
 #'
+#' @name cdiac
+#' @docType data
+#' @source \url{https://cdiac.ess-dive.lbl.gov/trends/temp/jonescru/data.html}
+#' @references \url{https://cdiac.ess-dive.lbl.gov/trends/temp/jonescru/data.html}
+#' @keywords data
 #' @format A data frame with 166 rows and 14 variables
 #' \describe{
-#'   \item{YEAR}{year}
-#'   \item{JAN, FEB, ..., DEC}{Anomaly for each month in the respective year}
-#'   \item{ANNUAL}{Annual anomaly for the year}
+#'   \item{year}{year}
+#'   \item{jan, feb, ..., dec}{Anomaly for each month in the respective year}
+#'   \item{annual}{Annual anomaly for the year}
 #' }
-#' @source \url{http://cdiac.ess-dive.lbl.gov/trends/temp/jonescru/data.html}
+#'

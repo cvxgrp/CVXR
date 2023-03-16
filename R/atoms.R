8fd97c4e6b2aaaed1cfd7a85ecc1c6add9e1e16c
Anqi Fu <anqi.fu.1987@gmail.com>
2023-03-16 14:42:51 -0400

    Finish adding general atoms

--- R/atoms.R
+++ R/atoms.R
@@ -276,7 +276,7 @@ setMethod("canonicalize", "Atom", function(object) {
 setMethod("graph_implementation", "Atom", function(object, arg_objs, dim, data = NA_real_) { stop("Unimplemented") })
 
 # .value_impl.Atom <- function(object) {
-#' @describeIn Atom Returns the value of each of the componets in an Atom. Returns an empty matrix if it's an empty atom
+#' @describeIn Atom Returns the value of each of the components in an Atom. Returns an empty matrix if it's an empty atom
 setMethod("value_impl", "Atom", function(object) {
   obj_dim <- dim(object)
   # dims with 0's dropped in presolve.
@@ -2379,9 +2379,10 @@ DiffPos <- function(x, y) {
 #' 
 #' the epigraph of the perspective transform of \eqn{f} can be given by
 #' 
-#' \deqn{\{(t, x, s) \in \mathbb{R}^{n+2} : t \geq sf(x/s) \} = \{ (t, x, s) : Fx + gt + se \in K \}},
+#' \deqn{\{(t, x, s) \in \mathbb{R}^{n+2} : t \geq sf(x/s) \} = \{ (t, x, s) : Fx + gt + se \in K \}}.
 #' 
-#' (see https://web.stanford.edu/~boyd/papers/pdf/sw_aff_ctrl.pdf).
+#' (See https://web.stanford.edu/~boyd/papers/pdf/sw_aff_ctrl.pdf).
+#' 
 #' Note that this is the perspective transform of a scalar expression viewed as
 #' a function of its underlying variables. The perspective atom does not return
 #' a `Callable`, so you cannot create compositions such as \eqn{p(g(x),s)}, where
@@ -2416,7 +2417,73 @@ setMethod("validate_args", "Perspective", function(object) {
   callNextMethod()
 })
 
-# TODO: FINISH ADDING PERSPECTIVE ATOM
+#' @param values A list of arguments to the atom.
+#' @describeIn Perspective Returns the perspective sf(x/s).
+setMethod("to_numeric", "Perspective", function(object, values) {
+  if(values[[1]] < 0)
+    stop("s must be nonnegative")
+  if(abs(values[[1]]) <= 1e-8)
+    stop("There are valid cases where s = 0, but we do not handle this yet, e.g., f(x) = x + 1")
+  
+  s_val <- values[[1]]
+  f <- object@f
+  
+  f_vars <- variables(f)
+  n_vars <- length(f_vars) 
+  old_x_vals <- lapply(f_vars, value)
+  
+  for(i in seq_len(n_vars))
+    value(f_vars[[i]]) <- values[[i+1]]/values[[1]]
+  
+  ret_val <- value(f)*s_val
+
+  for(i in seq_len(n_vars))
+    value(f_vars[[i]]) <- old_x_vals[[i]]
+  
+  return(ret_val)
+})
+
+#' @describeIn Perspective Returns the sign (is positive, is negative) of the atom.
+setMethod("sign_from_args", "Perspective", function(object) {
+  f_pos <- is_nonneg(object@f)
+  f_neg <- is_nonpos(object@f)
+  s_pos <- is_nonneg(object@args[[1]])
+  
+  if(!s_pos)
+    stop("s must be nonnegative")
+  
+  is_positive <- (f_pos && s_pos)
+  is_negative <- (f_neg && s_pos)
+  
+  return(c(is_positive, is_negative))
+})
+
+#' @describeIn Perspective Is the atom convex?
+setMethod("is_atom_convex", "Perspective", function(object) {
+  if(dpp_scope_active() && !is_param_free(object@f))
+    return(FALSE)
+  else
+    return(is_convex(object@f) && is_nonneg(object@args[[1]]))
+})
+
+#' @describeIn Perspective Is the atom concave?
+setMethod("is_atom_concave", "Perspective", function(object) {
+  if(dpp_scope_active() && !is_param_free(object@f))
+    return(FALSE)
+  else
+    return(is_concave(object@f) && is_nonneg(object@args[[1]]))
+})
+
+#' @param idx An index into the atom.
+#' @describeIn Perspective Is the atom weakly increasing in the argument \code{idx}?
+setMethod("is_incr", "Perspective", function(object, idx) { FALSE })
+
+#' @param idx An index into the atom.
+#' @describeIn Perspective Is the atom weakly decreasing in the argument \code{idx}?
+setMethod("is_decr", "Perspective", function(object, idx) { FALSE })
+
+#' @describeIn Perspective The dimensions of the atom.
+setMethod("dim_from_args", "Perspective", function(object) { dim(object@f) })
 
 #'
 #' The PfEigenvalue class.
@@ -2661,7 +2728,146 @@ setMethod(".grad", "QuadForm", function(object, values) {
   Matrix(as.vector(t(D)), sparse = TRUE)
 })
 
-# TODO: ADD SUPPFUNC ATOM.
+#'
+#' The SuppFuncAtom class.
+#' 
+#' This class represents the support function of an affine expression \eqn{y}.
+#'
+#' @slot y An \linkS4class{Expression} with \code{is_affine(y) == TRUE}.
+#' @slot parent A \linkS4class{SuppFunc} containing data for the convex set associated with this atom.
+#' @name SuppFuncAtom-class
+#' @aliases SuppFuncAtom
+#' @rdname SuppFuncAtom-class
+.SuppFuncAtom <- setClass("SuppFuncAtom", representation(y = "ConstValORExpr", parent = "SuppFunc", .eta = "numeric", .dim = "numeric"), contains = "Atom")
+
+#' @param y An \linkS4class{Expression} with \code{is_affine(y) == TRUE}.
+#' @param parent A \linkS4class{SuppFunc} containing data for the convex set associated with this atom.
+#' @rdname SuppFuncAtom-class
+SuppFuncAtom <- function(y, parent) { .SuppFuncAtom(y = y, parent = parent) }
+
+setMethod("initialize", "SuppFuncAtom", function(.Object, ..., y, parent) {
+  id(.Object) <- get_id()
+  # .Object@args <- list(as.Constant(y))
+  .Object@y <- y
+  .Object@parent <- parent
+  .Object@.eta <- NA_real_   # Store for debugging purposes.
+  .Object@.dim <- c()
+  validate_args()
+  callNextMethod(.Object, ..., atom_args = list(as.Constant(y)))
+})
+
+#' @describeIn SuppFuncAtom List of \linkS4class{Variable} objects in the atom.
+setMethod("variables", "SuppFuncAtom", function(object) { variables(object@args[[1]]) })
+
+#' @describeIn SuppFuncAtom List of \linkS4class{Parameter} objects in the atom.
+setMethod("parameters", "SuppFuncAtom", function(object) { list() })
+
+#' @describeIn SuppFuncAtom List of \linkS4class{Constant} objects in the atom.
+setMethod("constants", "SuppFuncAtom", function(object) { list() })
+
+#' @param object A \linkS4class{SuppFuncAtom} object.
+#' @describeIn SuppFuncAtom The dimensions of the atom.
+setMethod("dim_from_args", "SuppFuncAtom", function(object) { object@.dim })
+
+#' @describeIn SuppFuncAtom The sign (is positive, is negative) of the atom.
+setMethod("sign_from_args", "SuppFuncAtom", function(object) { c(FALSE, FALSE) })
+
+#' @describeIn SuppFuncAtom A logical value indicating whether the atom is nonnegative.
+setMethod("is_nonneg", "SuppFuncAtom", function(object) { FALSE })
+
+#' @describeIn SuppFuncAtom A logical value indicating whether the atom is nonpositive.
+setMethod("is_nonpos", "SuppFuncAtom", function(object) { FALSE })
+
+#' @describeIn SuppFuncAtom A logical value indicating whether the atom is imaginary.
+setMethod("is_imag", "SuppFuncAtom", function(object) { FALSE })
+
+#' @describeIn SuppFuncAtom A logical value indicating whether the atom is complex valued.
+setMethod("is_complex", "SuppFuncAtom", function(object) { FALSE })
+
+#' @describeIn SuppFuncAtom Is the atom convex?
+setMethod("is_atom_convex", "SuppFuncAtom", function(object) { TRUE })
+
+#' @describeIn SuppFuncAtom Is the atom concave?
+setMethod("is_atom_concave", "SuppFuncAtom", function(object) { FALSE })
+
+#' @describeIn SuppFuncAtom Is the atom log-log convex?
+setMethod("is_atom_log_log_convex", "SuppFuncAtom", function(object) { FALSE })
+
+#' @describeIn SuppFuncAtom Is the atom log-log concave?
+setMethod("is_atom_log_log_concave", "SuppFuncAtom", function(object) { FALSE })
+
+#' @describeIn SuppFuncAtom Is the atom quasiconvex?
+setMethod("is_atom_quasiconvex", "SuppFuncAtom", function(object) { TRUE })
+
+#' @describeIn SuppFuncAtom Is the atom quasiconcave?
+setMethod("is_atom_quasiconcave", "SuppFuncAtom", function(object) { FALSE })
+
+#' @param idx An index into the atom.
+#' @describeIn SuppFuncAtom Is the atom weakly increasing in argument \code{idx}?
+setMethod("is_incr", "SuppFuncAtom", function(object, idx) { FALSE })
+
+#' @describeIn SuppFuncAtom Is the atom weakly decreasing in argument \code{idx}?
+setMethod("is_decr", "SuppFuncAtom", function(object, idx) { FALSE })
+
+#' @describeIn SuppFuncAtom Is the atom convex?
+setMethod("is_convex", "SuppFuncAtom", function(object) { TRUE })
+
+#' @describeIn SuppFuncAtom Is the atom concave?
+setMethod("is_concave", "SuppFuncAtom", function(object) { FALSE })
+
+#' @describeIn SuppFuncAtom Is the atom log-log convex?
+setMethod("is_log_log_convex", "SuppFuncAtom", function(object) { FALSE })
+
+#' @describeIn SuppFuncAtom Is the atom log-log concave?
+setMethod("is_log_log_concave", "SuppFuncAtom", function(object) { FALSE })
+
+#' @describeIn SuppFuncAtom Is the atom quasiconvex?
+setMethod("is_quasiconvex", "SuppFuncAtom", function(object) { TRUE })
+
+#' @describeIn SuppFuncAtom Is the atom quasiconcave?
+setMethod("is_quasiconcave", "SuppFuncAtom", function(object) { FALSE })
+
+#' @describeIn SuppFuncAtom Returns the value of each of the components in the atom.
+setMethod("value_impl", "SuppFuncAtom", function(object) {
+  y_val <- value(object@args[[1]])
+  y_val <- round(y_val, digits = 9)
+  y_val <- as.vector(y_val)
+  x_flat <- as.vector(t(object@parent@x))
+  cons <- constraints(object@parent)
+  if(len(cons) == 0) {
+    dummy <- Variable()
+    cons <- list(dummy == 1)
+  }
+  
+  prob <- Problem(Maximize(y_val %*% x_flat), cons)
+  result <- solve(prob, solver = "SCS", eps = 1e-6)
+  return(result$value)
+})
+
+#' @param values A list of numeric values for the arguments
+#' @describeIn SuppFuncAtom Gives the (sub/super)gradient of the atom w.r.t. each variable
+setMethod(".grad", "SuppFuncAtom", function(object, values) {
+  # The implementation of .grad from LogDet was used as a reference for this implementation.
+  y0 <- value(object@args[[1]])   # Save for later.
+  y <- values[[1]]   # Ignore all other values.
+  value(object@args[[1]]) <- y
+  .value_impl(object)   # TODO: What does dead-store do? Check Python functionality in suppfunc.py.
+  value(object@args[[1]]) <- y0
+  gradval <- value(object@parent@x)
+  if(any(is.na(gradval)))
+    # If we evaluated the support function successfully, then this means the support function is not finite at this point.
+    return(list(NA_real_))
+  else {
+    gradmat <- t(sparseMatrix(as.vector(gradval)))
+    return(gradmat)
+  }
+})
+
+#' @describeIn SuppFuncAtom Strict inequalities are not allowed.
+setMethod("<",  signature(e1 = "SuppFuncAtom", e2 = "Expression"), function(e1, e2) { stop("Unimplemented: Strict inequalities are not allowed.") })
+
+#' @describeIn SuppFuncAtom Strict inequalities are not allowed.
+setMethod(">",  signature(e1 = "SuppFuncAtom", e2 = "Expression"), function(e1, e2) { stop("Unimplemented: Strict inequalities are not allowed.") })
 
 #'
 #' The SymbolicQuadForm class.
@@ -3132,7 +3338,7 @@ setMethod("initialize", "TrInv", function(.Object, ..., X) {
 #' @describeIn TrInv The trace of the inverse of a positive definite matrix.
 setMethod("to_numeric", "TrInv", function(object, values) {
   # If values[[1]] isn't Hermitian, return Inf.
-  if(base::norm(values[[1]] - Conj(t(values[[1]])), "F") >= 10^-8)
+  if(base::norm(values[[1]] - Conj(t(values[[1]])), "F") >= 1e-8)
     return(Inf)
   # Take symmetric part of the input to enhance numerical stability.
   symm <- (values[[1]] + t(values[[1]]))/2

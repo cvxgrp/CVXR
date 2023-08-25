753423eaeab0c30e688895998c4c0974ff547dfb
Anqi Fu <anqif@alumni.stanford.edu>
2023-08-25 19:14:56 -0400

    Finish updating dgp2dcp functions and classes

--- R/dgp2dcp.R
+++ R/dgp2dcp.R
@@ -551,6 +551,10 @@ DgpCanonMethods <- function(...) { .DgpCanonMethods(...) }
 #' @describeIn DgpCanonMethods Returns the name of all the canonicalization methods
 setMethod("names", signature(x = "DgpCanonMethods"), function(x) { names(Dgp2Dcp.CANON_METHODS) })
 
+setMethod("match", signature(x = "character", table = "DgpCanonMethods"), function(x, table, nomatch = NA_integer_, incomparables = NULL) {
+  return(x %in% names(Dgp2Dcp.CANON_METHODS))
+})
+
 # TODO: How to implement this with S4 setMethod? Signature is x = "DgpCanonMethods", i = "character", j = "missing".
 '[[.DgpCanonMethods' <- function(x, i, j, ..., exact = TRUE) { do.call("$", list(x, i)) }
 
@@ -558,22 +562,38 @@ setMethod("names", signature(x = "DgpCanonMethods"), function(x) { names(Dgp2Dcp
 #' @describeIn DgpCanonMethods Returns either a canonicalized variable or 
 #'  a corresponding Dgp2Dcp canonicalization method 
 setMethod("$", signature(x = "DgpCanonMethods"), function(x, name) {
-  if(name == "Variable") {
-    # TODO: Check scoping of x here is correct.
-    variable_canon <- function(variable, args) {
-      args <- NULL
-      vid <- as.character(variable@id)
-      # Swap out positive variables for unconstrained variables.
-      if(vid %in% names(x@.variables))
-        return(list(x@.variables[[vid]], list()))
-      else {
-        # log_variable <- Variable(dim(variable), id = variable@id)
-        log_variable <- new("Variable", dim = dim(variable), id = variable@id)
-        x@.variables[[vid]] <- log_variable
-        return(list(log_variable, list()))
-      }
-    }
-    return(variable_canon)
-  } else
+  if(name == "Variable")
+    return(function(...) { variable_canon(x, ...) })
+  else if(name == "Parameter")
+    return(function(...) { parameter_canon(x, ...) })
+  else
     return(Dgp2Dcp.CANON_METHODS[[name]])
+})
+
+setMethod("variable_canon", "DgpCanonMethods", function(object, variable, args) {
+  # args <- NULL
+  # Swaps out positive variables for unconstrained variables.
+  vid <- id(variable)
+  vid_char <- as.character(vid)
+  if(vid_char %in% names(object@.variables))
+    return(list(object, object@.variables[[vid_char]], list()))
+  else {
+    log_variable <- new("Variable", dim = dim(variable), var_id = vid)
+    object@.variables[[vid_char]] <- log_variable
+    return(list(object, log_variable, list()))
+  }
+})
+
+setMethod("parameter_canon", "DgpCanonMethods", function(object, parameter, args) {
+  # args <- NULL
+  # Swaps out positive parameters for unconstrained variables.
+  pid <- id(parameter)
+  pid_char <- as.character(pid)
+  if(pid_char %in% names(object@.parameters))
+    return(list(object, object@.parameters[[pid_char]], list()))
+  else {
+    log_parameter <- new("Parameter", dim = dim(parameter), name = name(parameter), value = base::log(value(parameter)))
+    object@.parameters[[pid_char]] <- log_parameter
+    return(list(object, log_parameter, list()))
+  }
 })

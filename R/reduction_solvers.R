f979d9061ee564ee0d7787c3f2f3407ceeeb49d2
Anqi Fu <fua@mskcc.org>
2023-09-18 11:32:25 -0400

    WIP Updating solvers classes and functions under reductions. Currently finished main utilities and bisection methods

--- R/reduction_solvers.R
+++ R/reduction_solvers.R
@@ -1,16 +1,47 @@
-# Solver utility functions.
+######################################################################
+#                      Solver utility functions.
+######################################################################
 
 #'
-#' Organize the constraints into a dictionary keyed by constraint names.
+#' Stacks the values of the given variables.
 #'
-#' @param constraints a list of constraints.
-#' @return A list of constraint types where constr_map[[cone_type]] maps to a list.
-group_constraints <- function(constraints) {
-  constr_map <- list()
-  for(c in constraints)
-    constr_map[[class(c)]] <- c(constr_map[[class(c)]], c)
-  return(constr_map)
+#' @param variables a list of variables.
+#' @param default value to use when variable value is NA
+#' @param byrow logical value indicating whether to fill the matrix by rows. Defaults to FALSE.
+stack_vals <- function(variables, default, byrow = FALSE) {
+  value <- list()
+  for(variable in variables) {
+    val <- value(variable)
+    if(is.na(val))   # Unknown values.
+      mat <- matrix(rep(default, size(variable)), ncol = 1)
+    else
+      mat <- matrix(val, ncol = 1, byrow = byrow)
+    value <- c(value, list(mat))
+  }
+  # return(do.call("c", value))
+  return(do.call("rbind", value))
 }
+
+expcone_permutor <- function(n_cones, exp_cone_order) {
+  if(n_cones == 0)
+    return(c())
+  order <- rep(exp_cone_order, n_cones)   # e.g., c(1,0,2, 1,0,2, 1, 0,2, ...)
+  offsets <- 3*do.call("c", lapply(seq(n_cones), function(i) { rep(i-1, 3) }))   # e.g., c(0,0,0, 3,3,3, 6,6,6, ...)
+  perm <- order + offsets
+  return(perm)
+}
+
+# #'
+# #' Organize the constraints into a dictionary keyed by constraint names.
+# #'
+# #' @param constraints a list of constraints.
+# #' @return A list of constraint types where constr_map[[cone_type]] maps to a list.
+# group_constraints <- function(constraints) {
+#  constr_map <- list()
+#  for(c in constraints)
+#    constr_map[[class(c)]] <- c(constr_map[[class(c)]], c)
+#  return(constr_map)
+# }
 
 #'
 #' Gets a specified value of a dual variable.
@@ -40,7 +71,7 @@ get_dual_values <- function(result_vec, parse_func, constraints) {
   for(constr in constraints) {
     # TODO: Reshape based on dual variable size.
     parsed <- parse_func(result_vec, offset, constr)
-    dual_vars[[as.character(constr@id)]] <- parsed[[1]]
+    dual_vars[[as.character(id(constr))]] <- parsed[[1]]
     offset <- parsed[[2]]
   }
   return(dual_vars)
@@ -331,4 +362,170 @@ setMethod("construct_intermediate_chain", signature(problem = "Problem", candida
     stop("Problem could not be reduced to a QP, and no conic solvers exist among candidate solvers")
   reductions <- c(reductions, list(Dcp2Cone(), CvxAttr2Constr()))
   return(Chain(reductions = reductions))
+})
+
+################################################
+#              BISECTION
+###############################################
+Bisection.lower_problem <- function(problem) {
+  # Evaluates lazy constraints
+  Problem(Minimize(0), c(problem@constraints, lapply(problem@.lazy_constraints, function(con) { con() })))
+}
+
+Bisection.infeasible <- function(problem, result) {
+  # is.null(problem) || problem@status %in% c(INFEASIBLE, INFEASIBLE_INACCURATE)
+  is.null(problem) || result$status %in% c(INFEASIBLE, INFEASIBLE_INACCURATE)
+}
+
+# TODO: Eliminate the need for reconstructing the problem (via Bisection.lower_problem). 
+# Right now, some constraints are lazy, namely the callable constraints, because for 
+# certain values of the parameters they become invalidated. Either support lazy 
+# constraints that do not need recompilation, or (if possible) find rewritings that 'just work'.
+Bisection.find_bisection_interval <- function(problem, t, solver = NULL, low = NA_real_, high = NA_real_, max_iters = 100) {
+  # Finds an interval for bisection
+  if(is.na(low))
+    low <- ifelse(is_nonneg(t), 0, -1)
+  if(is.na(high))
+    high <- ifelse(is_nonpos(t), 0, 1)
+  
+  infeasible_low <- is_nonneg(t)
+  feasible_high <- is_nonpos(t)
+  for(i in seq(max_iters)) {
+    if(!feasible_high) {
+      value(t) <- high
+      lowered <- Bisection.lower_problem(problem)
+      result <- solve(lowered, solver = solver)
+      if(Bisection.infeasible(lowered, result)) {
+        low <- high
+        high <- 2*high
+        next
+      } else if(result$status %in% SOLUTION_PRESENT)
+        feasible_high <- TRUE
+      else
+        stop("Solver failed with status ", result$status)
+    }
+    
+    if(!infeasible_low) {
+      value(t) <- low
+      lowered <- Bisection.lower_problem(problem)
+      result <- solve(lowered, solver = solver)
+      if(Bisection.infeasible(lowered, result))
+        infeasible_low <- TRUE
+      else if(result$status %in% SOLUTION_PRESENT) {
+        high <- low
+        low <- 2*low
+        next
+      } else
+        stop("Solver failed with status ", result$status)
+    }
+    
+    if(infeasible_low && feasible_high)
+      return(c(low, high))
+  }
+  
+  stop("Unable to find suitable interval for bisection; your problem may be unbounded.")
+}
+
+Bisection.bisect_int <- function(problem, solver, t, low, high, tighten_lower, tighten_higher, eps = 1e-6, verbose = FALSE, max_iters = 100) {
+  # Bisect problem on the parameter t
+  verbose_freq <- 5
+  soln <- NA_real_
+  
+  for(i in seq(max_iters)) {
+    if(low > high)
+      stop("low must be less than or equal to high")
+    
+    if(!is.na(soln) && (high - low) <= eps) {
+      # the previous iteration might have been infeasible, but
+      # the tighten* functions might have narrowed the interval
+      # to the optimal value in the previous iteration (hence the
+      # soln is not None check)
+      return(c(soln, low, high))
+    }
+    
+    query_pt <- (low + high) / 2.0
+    if(verbose && i %% verbose_freq == 0) {
+      print(paste("(iteration ", i, ") lower bound: ", low, sep = ""))
+      print(paste("(iteration ", i, ") upper bound: ", high, sep = ""))
+      print(paste("(iteration ", i, ") query point: ", query_pt, sep = ""))
+    }
+    value(t) <- query_pt
+    lowered <- Bisection.lowered_problem(problem)
+    result <- solve(lowered, solver = solver)
+    
+    if(Bisection.infeasible(lowered, result)) {
+      if(verbose && i %% verbose_freq == 0)
+        print(paste("(iteration ", i, ") query was infeasible"))
+      low <- tighten_lower(query_pt)
+    } else if(result$status %in% SOLUTION_PRESENT) {
+      if(verbose && i %% verbose_freq == 0)
+        print(paste("(iteration ", i, ") query was feasible with solution ", result$solution, sep = ""))
+      soln <- result$solution
+      high <- tighten_higher(query_pt)
+    } else {
+      if(verbose)
+        print("Aborting; the solver failed...")
+      stop("Solver failed with status ", result$status)
+    }
+  }
+}
+
+#'
+#' Bisection on a one-parameter family of DCP problems.
+#' 
+#' Bisects on a one-parameter family of DCP problems emitted by Dqcp2Dcp.
+#' 
+#' @param problem A \linkS4class{Problem} emitted by Dqcp2Dcp.
+#' @param solver The solver to use for bisection.
+#' @param low (Optional) Lower bound for bisection.
+#' @param high (Optional) Upper bound for bisection.
+#' @param eps Terminate bisection when width of interval is strictly less than eps.
+#' @param verbose A logical value indicating whether to print verbose output related to bisection.
+#' @param max_iters The maximum number of iterations to run the bisection.
+#' @return A \linkS4class{Solution} object.
+setMethod("bisect", "Problem", function(problem, solver = NULL, low = NA_real_, high = NA_real_, eps = 1e-6, verbose = FALSE, max_iters = 100, max_iters_interval_search = 100) {
+  if(!.hasSlot(problem, ".bisection_data"))
+    stop("bisect only accepts problems emitted by Dqcp2Dcp (with .bisection_data slot)")
+  tmp <- problem@.bisection_data
+  feas_problem <- tmp[[1]]
+  t <- tmp[[2]]
+  tighten_lower <- tmp[[3]]
+  tighten_higher <- tmp[[4]]
+  
+  if(verbose)
+    cat("\n******************************************************",
+        "**************************\n",
+        "Preparing to bisect problem\n\n", as.character(Bisection.lower_problem(problem)), "\n", sep = "")
+  
+  lowered_feas <- Bisection.lower_problem(feas_problem)
+  result <- solve(lowered_feas, solver = solver)
+  if(Bisection.infeasible(lowered_feas, result)) {
+    if(verbose)
+      print("Problem is infeasible")
+    return(failure_solution(INFEASIBLE))
+  }
+  
+  if(is.na(low) || is.na(high)) {
+    if(verbose)
+      print("Finding interval for bisection...")
+    tmp <- Bisection.find_bisection_interval(problem, t, solver, low, high, max_iters_interval_search)
+    low <- tmp[[1]]
+    high <- tmp[[2]]
+  }
+  
+  if(verbose) {
+    print(paste("initial lower bound:", low))
+    print(paste("initial upper bound:", high))
+  }
+  
+  tmp <- Bisection.bisect_int(problem, solver, t, low, high, tighten_lower, tighten_higher, eps, verbose, max_iters)
+  soln <- tmp[[1]]
+  low <- tmp[[2]]
+  high <- tmp[[3]]
+  soln@opt_val <- (low + high) / 2.0
+  if(verbose)
+    cat("Bisection completed, with lower bound ", low, " and upper bound ", high,
+          "\n******************************************",
+          "**************************************\n", sep = "")
+  return(soln)
 })

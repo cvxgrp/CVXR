385737dd91dfde12db51d63bf61e0dc3821bbbc0
Anqi Fu <anqif@alumni.stanford.edu>
2023-09-19 13:03:03 -0400

    Finish updating main reduction solver functions and classes

--- R/reduction_solvers.R
+++ R/reduction_solvers.R
@@ -302,59 +302,122 @@ SolvingChain.reductions_for_problem_class(problem, candidates, gp = FALSE, solve
 #' @param canon_backend Specifies which backend to use for canonicalization, which can affect compilation time. Defaults to NA, i.e., selecting the default backend ("CPP").
 #' @param solver_opts List of additional arguments to pass to the solver. 
 #' @return A \linkS4class{SolvingChain} that can be used to solve the problem.
-construct_solving_chain <- function(problem, candidates) {
-  reductions <- list()
-  if(length(parameters(problem)) > 0)
-    reductions <- c(reductions, EvalParams())
-  if(length(variables(problem)) == 0) {
-    reductions <- c(reductions, ConstantSolver())
-    return(SolvingChain(reductions = reductions))
+construct_solving_chain <- function(problem, candidates, gp = FALSE, enforce_dpp = FALSE, ignore_dpp = FALSE, canon_backend = NA_character_, solver_opts = NULL) {
+  if(length(variables(problem)) == 0)
+    return(SolvingChain(reductions = list(ConstantSolver())))
+  reductions <- SolvingChain.reductions_for_problem_class(problem, candidates, gp, solver_opts)
+  
+  # Process DPP status of the problem.
+  dpp_context <- ifelse(gp, "dgp", "dcp")
+  dpp_error_msg <- paste("You are solving a parameterized problem that is not DPP. ",
+                         "Because the problem is not DPP, subsequent solves will not be "
+                         "faster than the first one. For more information, see the "
+                         "documentation on Discplined Parametrized Programming, at\n"
+                         "\thttps://www.cvxpy.org/tutorial/advanced/index.html#"
+                         "disciplined-parametrized-programming", sep = "")
+  
+  if(ignore_dpp || !is_dpp(problem, dpp_context)) {
+    # No warning for ignore_dpp.
+    if(ignore_dpp)
+      reductions <- c(list(EvalParams()), reductions)
+    else if(!enforce_dpp) {
+      warning(dpp_error_msg)
+      reductions <- c(list(EvalParams()), reductions)
+    } else
+      stop(dpp_error_msg)
+  } else if(any(sapply(parameters(problem), is_complex)))
+    reductions <- c(list(EvalParams()), reductions)
+  else {   # Compilation with DPP.
+    n_parameters <- sum(parameters(problem), function(param) { prod(dim(param)) })
+    if(n_parameters >= PARAM_THRESHOLD)
+      warning("Your problem has too many parameters for efficient DPP compilation. We suggest setting ignore_dpp = TRUE")
   }
-
-  # Conclude the chain with one of the following:
+  
+  # Conclude with matrix stuffing; choose one of the following paths:
   #   1) QpMatrixStuffing -> [a QpSolver]
   #   2) ConeMatrixStuffing -> [a ConicSolver]
-
-  # First, attempt to canonicalize the problem to a linearly constrained QP.
-  if(length(candidates$qp_solvers) > 0 && accepts(QpMatrixStuffing(), problem)) {
-    idx <- sapply(candidates$qp_solvers, function(s) { min(which(QP_SOLVERS == s)) })
-    sorted_candidates <- candidates$qp_solvers[order(idx)]
-    solver <- sorted_candidates[[1]]
+  if(!is.null(solver_opts) && "use_quad_obj" %in% names(solver_opts))
+    use_quad <- solver_opts$use_quad_obj
+  else
+    use_quad <- TRUE
+  
+  if(SolvingChain.solve_as_qp(problem, candidates) && use_quad) {
+    # Canonicalize as a QP.
+    solver <- candidates$qp_solvers[[1]]
     solver_instance <- SOLVER_MAP_QP[[solver]]
-    reductions <- c(reductions, list(QpMatrixStuffing(), solver_instance))
+    reductions <- c(reductions, list(QpMatrixStuffing(canon_backend = canon_backend), solver_instance))
     return(SolvingChain(reductions = reductions))
   }
-
+  
+  # Canonicalize as a cone program.
   if(length(candidates$conic_solvers) == 0)
     stop(paste("Problem could not be reduced to a QP, and no conic solvers exist among candidate solvers (",
                paste(unlist(candidates), collapse = ","), ")", sep = ""))
-
-  # Our choice of solver depends upon which atoms are present in the problem.
-  # The types of atoms to check for are SOC atoms, PSD atoms, and exponential atoms.
-  atoms <- atoms(problem)
+  
+  # We use constr_types to infer an incomplete list of cones that the solver will need after canonicalization.
+  constr_types <- sapply(problem@constraints, class)
+  constr_types <- unique(constr_types)
+  ex_cos <- constr_types[constr_types %in% EXOTIC_CONES]   # The way we populate ex_cos will need to change if and when we have atoms that require exotic cones.
+  approx_cos <- constr_types[constr_types %in% APPROX_CONES]
+  for(co in ex_cos) {
+    sim_cos <- EXOTIC_CONES[[co]]   # Get the set of required simple cones.
+    if(!(app_cos %in% constr_types))
+      constr_types <- c(constr_types, app_cos)
+    constr_types <- constr_types[which(constr_types != co)]
+  }
+  
+  # We now go over individual elementary cones support by CVXR (SOC, ExpCone, 
+  # NonNegConstraint, ZeroConstraint, PSDConstraint, PowCone3D) and check if
+  # they've appeared in constr_types or if the problem has an atom requiring 
+  # that cone.
   cones <- c()
-  if(any(atoms %in% SOC_ATOMS) || any(sapply(problem@constraints, inherits, what = "SOC")))
+  atoms <- atoms(problem)
+  if("SOC" %in% constr_types || any(atoms %in% SOC_ATOMS))
     cones <- c(cones, "SOC")
-  if(any(atoms %in% EXP_ATOMS) || any(sapply(problem@constraints, inherits, what = "ExpCone")))
+  if("ExpCone" %in% constr_types || any(atoms %in% EXP_ATOMS))
     cones <- c(cones, "ExpCone")
-  if(any(atoms %in% PSD_ATOMS) || any(sapply(problem@constraints, inherits, what = "PSDConstraint"))
-                               || any(sapply(variables(problem), function(v) { is_psd(v) || is_nsd(v) })))
+  if(any(sapply(c("IneqConstraint", "NonPosConstraint", "NonNegConstraint"), function(t) { t %in% constr_types } )) || any(atoms %in% NONPOS_ATOMS))
+    cones <- c(cones, "NonNegConstraint")
+  if("EqConstraint" %in% constr_types || "ZeroConstraint" %in% constr_types)
+    cones <- c(cones, "ZeroConstraint")
+  if("PSDConstraint" %in% constr_types || any(atoms %in% PSD_ATOMS) || any(variables(problem), function(v) { is_psd(v) || is_nsd(v) }))
     cones <- c(cones, "PSDConstraint")
+  if("PowCone3D" %in% constr_types) {
+    # If we add in atoms that specifically use the 3D power cone (rather than 
+    # the ND power cone), then we'll need to check for those atoms here as well.
+    cones <- c(cones, "PowCone3D")
+  }
 
   # Here, we make use of the observation that canonicalization only
   # increases the number of constraints in our problem.
   has_constr <- length(cones) > 0 || length(problem@constraints) > 0
-
-  idx <- sapply(candidates$conic_solvers, function(s) { min(which(CONIC_SOLVERS == s)) })
-  sorted_candidates <- candidates$conic_solvers[order(idx)]
-  for(solver in sorted_candidates) {
+  
+  for(solver %in% candidates$conic_solvers) {
     solver_instance <- SOLVER_MAP_CONIC[[solver]]
-    if(all(cones %in% supported_constraints(solver_instance)) && (has_constr || !requires_constr(solver_instance))) {
-      reductions <- c(reductions, list(ConeMatrixStuffing(), solver_instance))
+    
+    # Cones supported for MI problems may differ from non MI.
+    if(is_mixed_integer(problem))
+      supported_constraints <- mi_supported_constraints(solver_instance)
+    else
+      supported_constraints <- supported_constraints(solver_instance)
+    
+    if(all(cones %in% supported_constraints) && (has_constr || !requires_constr(solver_instance))) {
+      if(length(ex_cost) > 0)
+        reductions <- c(reductions, list(Exotic2Common()))
+      if("RelEntrConeQuad" %in% approx_cos || "OpRelEntrConeQuad" %in% approx_cos)
+        reductions <- c(reductions, list(QuadApprox()))
+      
+      # Should the objective be canonicalized to a quadratic?
+      if(!is.null(solver_opts) && "use_quad_obj" %in% names(solver_opts))
+        use_quad_obj <- solver_opts$use_quad_obj
+      else
+        use_quad_obj <- TRUE
+      quad_obj <- use_quad_obj && supports_quad_obj(solver_instance) && has_quadratic_term(problem@objective@expr)
+      reductions <- c(reductions, list(Dcp2Cone(quad_obj = quad_obj), CvxAttr2Constr(), ConeMatrixStuffing(quad_obj = quad_obj, canon_backend = canon_backend), solver_instance))
       return(SolvingChain(reductions = reductions))
     }
   }
-
+  
   stop(paste("Either candidate conic solvers (",
        paste(candidates$conic_solvers, sep = " ", collapse = ","), ") do not support the cones output by the problem (",
        paste(cones, sep = " ", collapse = ","), "), or there are not enough constraints in the problem.", sep = ""))
@@ -394,28 +457,22 @@ setMethod("prepend", signature(object = "SolvingChain", chain = "Chain"), functi
 #' @param problem The problem to solve.
 #' @param warm_start A boolean of whether to warm start the solver.
 #' @param verbose A boolean of whether to enable solver verbosity.
-#' @param feastol The feasible tolerance.
-#' @param reltol The relative tolerance.
-#' @param abstol The absolute tolerance.
-#' @param num_iter The maximum number of iterations.
 #' @param solver_opts A list of Solver specific options
 #' @describeIn SolvingChain Applies each reduction in the chain to the problem, solves it,
 #' and then inverts the chain to return a solution of the supplied problem.
-setMethod("reduction_solve", signature(object = "SolvingChain", problem = "Problem"), function(object, problem, warm_start, verbose, feastol, reltol, abstol, num_iter, solver_opts) {
+setMethod("reduction_solve", signature(object = "SolvingChain", problem = "Problem"), function(object, problem, warm_start, verbose, solver_opts) {
   tmp <- perform(object, problem)
   object <- tmp[[1]]
   data <- tmp[[2]]
   inverse_data <- tmp[[3]]
-  solution <- solve_via_data(object@solver, data, warm_start, verbose, feastol, reltol, abstol, num_iter, solver_opts)
+  solution <- solve_via_data(object@solver, data, warm_start, verbose, solver_opts)
   return(invert(object, solution, inverse_data))
 })
 
 #' @param data Data for the solver.
 #' @describeIn SolvingChain Solves the problem using the data output by the an apply invocation.
-setMethod("reduction_solve_via_data", "SolvingChain", function(object, problem, data, warm_start, verbose,
-                                                               feastol, reltol, abstol, num_iter, solver_opts) {
-  return(solve_via_data(object@solver, data, warm_start, verbose,
-                        feastol, reltol, abstol, num_iter, solver_opts, problem@.solver_cache))
+setMethod("reduction_solve_via_data", "SolvingChain", function(object, problem, data, warm_start, verbose, solver_opts = list()) {
+  return(solve_via_data(object@solver, data, warm_start, verbose, solver_opts, problem@.solver_cache))
 })
 
 #'

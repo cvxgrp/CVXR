cfe4e981d69c26fb4e76e4291b7772896e4af00b
Anqi Fu <fua@mskcc.org>
2023-10-04 14:04:20 -0400

    WIP Finished everything except op rel cone quad tests in cone2cone

--- tests/testthat/test_g01-cone2cone.R
+++ tests/testthat/test_g01-cone2cone.R
@@ -338,7 +338,7 @@ test_that("TestSlacks: test lp 3", {
   }
 })
 
-test_that("test lp 4", {
+test_that("TestSlacks: test lp 4", {
   # Infeasible LP.
   sth <- lp_4()
   for(affine in AFF_LP_CASES) {
@@ -347,7 +347,7 @@ test_that("test lp 4", {
   }
 })
 
-test_that("test socp 2", {
+test_that("TestSlacks: test socp 2", {
   sth <- socp_2()
   for(affine in AFF_SOCP_CASES) {
     result <- TestSlacks.simulate_chain(sth@prob, affine, solver = "ECOS")
@@ -356,7 +356,7 @@ test_that("test socp 2", {
   }
 })
 
-test_that("test socp 3", {
+test_that("TestSlacks: test socp 3", {
   for(axis in c(1, 2)) {
     sth <- socp_3(axis)
     result <- TestSlacks.simulate_chain(sth@prob, affine, solver = "ECOS")
@@ -365,7 +365,7 @@ test_that("test socp 3", {
   }
 })
 
-test_that("test expcone 1", {
+test_that("TestSlacks: test expcone 1", {
   sth <- expcone_1()
   for(affine in AFF_EXP_CASES) {
     result <- TestSlacks.simulate_chain(sth@prob, affine, solver = "ECOS")
@@ -374,7 +374,7 @@ test_that("test expcone 1", {
   }
 })
 
-test_that("test expcone socp 1", {
+test_that("TestSlacks: test expcone socp 1", {
   sth <- expcone_socp_1()
   for(affine in AFF_MIXED_CASES) {
     result <- TestSlacks.simulate_chain(sth@prob, affine, solver = "ECOS")
@@ -383,7 +383,7 @@ test_that("test expcone socp 1", {
   }
 })
 
-test_that("test pcp 1", {
+test_that("TestSlacks: test pcp 1", {
   sth <- pcp_1()
   for(affine in AFF_PCP_CASES) {
     result <- TestSlacks.simulate_chain(sth@prob, affine, solver = "SCS", eps = 1e-8)
@@ -392,7 +392,7 @@ test_that("test pcp 1", {
   }
 })
 
-test_that("test pcp 2", {
+test_that("TestSlacks: test pcp 2", {
   sth <- pcp_2()
   for(affine in AFF_PCP_CASES) {
     result <- TestSlacks.simulate_chain(sth@prob, affine, solver = "SCS", eps = 1e-8)
@@ -401,7 +401,7 @@ test_that("test pcp 2", {
   }
 })
 
-test_that("test mi lp 1", {
+test_that("TestSlacks: test mi lp 1", {
   sth <- mi_lp_1()
   for(affine in AFF_LP_CASES) {
     result <- TestSlacks.simulate_chain(sth@prob, affine, solver = "ECOS_BB")
@@ -411,7 +411,7 @@ test_that("test mi lp 1", {
 })
 
 # TODO: Known bug in ECOS BB.
-# test_that("test mi socp 1", {
+# test_that("TestSlacks: test mi socp 1", {
 #  sth <- mi_socp_1()
 #  for(affine in AFF_SOCP_CASES) {
 #    result <- TestSlacks.simulate_chain(sth@prob, affine, solver = "ECOS_BB")
@@ -420,7 +420,7 @@ test_that("test mi lp 1", {
 #  }
 # })
 
-test_that("test mi socp 2", {
+test_that("TestSlacks: test mi socp 2", {
   can_solve <- FALSE
   for(svr in INSTALLED_MI) {
     if(svr %in% MI_SOCP && svr != ECOS_BB) {
@@ -442,5 +442,195 @@ test_that("test mi socp 2", {
   }
 })
 
-# TODO: Add rest of cone2cone tests and classes like TestSlacks, TestPowND, etc.
+##############
+#            #
+# Test PowND #
+#            #
+##############
+
+TestPowND.pcp_3 <- function(axis) {
+  # A modification of pcp_2. Reformulate
+  #
+  #         max  (x^0.2)*(y^0.8) + z^0.4 - x
+  #          s.t. x + y + z/2 == 2
+  #               x, y, z >= 0
+  #      Into
+  #
+  #          max  x3 + x4 - x0
+  #          s.t. x0 + x1 + x2 / 2 == 2,
+  #
+  #               W := [[x0, x2],
+  #                     [x1, 1.0]]
+  #               z := [x3, x4]
+  #               alpha := [[0.2, 0.4],
+  #                         [0.8, 0.6]]
+  #               (W, z) in PowND(alpha, axis = 2)
+  x <- Variable(3)
+  expect_x <- c(0.06393515, 0.78320961, 2.30571048)
+  hypos <- Variable(2)
+  expect_hypos <- NULL
+  objective <- Maximize(sum(hypos) - x[1])
+  W <- bmat(list(list(x[1], x[3]),
+                 list(x[2], 1.0)))
+  alpha <- rbind(c(0.2, 0.4), c(0.8, 0.6))
+  
+  if(axis == 1) {
+    W <- t(W)
+    alpha <- t(alpha)
+  }
+  
+  con_pairs <- list(list(x[1] + x[2] + 0.5*x[3] == 2, NULL),
+                    list(PowConeND(W, hypos, alpha, axis = axis), NULL))
+  obj_pairs <- list(objective, 1.8073406786220672)
+  var_pairs <- list(list(x, expect_x),
+                    list(hypos, expect_hypos))
+  sth <- SolverTestHelper(obj_pair, var_pairs, con_pairs)
+  return(sth)
+}
+
+test_that("TestPowND: test pcp 3a", {
+  sth <- TestPowND.pcp_3(axis = 2)
+  result <- solve(sth, solver = "SCS", eps = 1e-8)
+  verify_objective(sth, result, tolerance = 1e-3)
+  verify_primal_values(sth, result, tolerance = 1e-3)
+  check_complementarity(sth, result, tolerance = 1e-3)
+})
+
+test_that("TestPowND: test pcp 3b", {
+  sth <- TestPowND.pcp_3(axis = 1)
+  result <- solve(sth, solver = "SCS", eps = 1e-8)
+  verify_objective(sth, result, tolerance = 1e-3)
+  verify_primal_values(sth, result, tolerance = 1e-3)
+  check_complementarity(sth, result, tolerance = 1e-3)
+})
+
+TestPowND.pcp_4 <- function(ceei = TRUE) {
+  # A power cone formulation of a Fisher market equilibrium pricing model.
+  # ceei = Competitive Equilibrium from Equal Incomes.
+  
+  # Generate test data.
+  set.seed(0)
+  n_buyer <- 4
+  n_items <- 6
+  V <- matrix(runif(n_buyer*n_items), nrow = n_buyer, ncol = n_items)
+  X <- Variable(n_buyer, n_items, nonneg = TRUE)
+  u <- sum(multiply(V, X), axis = 1)
+  
+  if(ceei)
+    b <- rep(1, n_buyer)/n_buyer
+  else
+    b <- c(0.3, 0.15, 0.2, 0.35)
+  
+  log_objective <- Maximize(sum(multiply(b, log(u))))
+  log_cons <- list(sum(X, axis = 2) <= 1)
+  log_prob <- Problem(log_objective, log_cons)
+  result <- solve(log_prob, solver = "SCS", eps = 1e-8)
+  expect_X <- result$getValue(X)
+  
+  z <- Variable()
+  pow_objective <- list(Maximize(z), exp(result$getValue(log_prob)))
+  pow_cons <- list(list(sum(X, axis = 2) <= 1, NULL),
+                   list(PowConeND(W = u, z = z, alpha = b), NULL))
+  pow_vars <- list(list(X, expect_X))
+  sth <- SolverTestHelper(pow_objective, pow_vars, pow_cons)
+  return(sth)
+}
+
+test_that("TestPowND: test pcp 4a", {
+  sth <- TestPowND.pcp_4(ceei = TRUE)
+  result <- solve(sth, solver = "SCS", eps = 1e-8)
+  verify_objective(sth, result, tolerance = 1e-3)
+  verify_primal_values(sth, result, tolerance = 1e-3)
+  check_complementarity(sth, result, tolerance = 1e-3)
+})
+
+test_that("TestPowND: test pcp 4b", {
+  sth <- TestPowND.pcp_4(ceei = FALSE)
+  result <- solve(sth, solver = "SCS", eps = 1e-8)
+  verify_objective(sth, result, tolerance = 1e-3)
+  verify_primal_values(sth, result, tolerance = 1e-3)
+  check_complementarity(sth, result, tolerance = 1e-3)
+})
+
+######################
+#                    #
+# Test Rel Entr Quad #
+#                    #
+######################
+
+TestRelEntrQuad.expcone_1 <- function() {
+  # min   3 * x[1] + 2 * x[2] + x[3]
+  # s.t.  0.1 <= x[1] + x[2] + x[3] <= 1
+  #       x >= 0
+  #       and ...
+  #         x[1] >= x[2] * exp(x[3] / x[2])
+  #       equivalently ...
+  #         x[1] / x[2] >= exp(x[3] / x[2])
+  #         log(x[1] / x[2]) >= x[3] / x[2]
+  #         x[2] log(x[2] / x[1]) <= -x[3]
+  
+  x <- Variable(3, 1)
+  cone_con <- as_quad_approx(ExpCone(x[3], x[2], x[1]), 5, 5)
+  constraints <- list(sum(x) <= 1.0, sum(x) >= 0.1, x >= 0, cone_con)
+  obj <- Minimize(3*x[1] + 2*x[2] + x[3])
+  obj_pair <- list(obj, 0.23534820622420757)
+  expect_exp <- list(c(-1.35348213), c(-0.35348211), c(0.64651792))
+  con_pairs <- list(list(constraints[[1]], 0),
+                    list(constraints[[2]], 2.3534821130067614),
+                    list(constraints[[3]], matrix(0, nrow = 3, ncol = 1)),
+                    list(constraints[[4]], expect_exp))
+  expect_x <- matrix(c(0.05462721, 0.02609378, 0.01927901), nrow = 3, ncol = 1)
+  var_pairs <- list(x, expect_x)
+  sth <- SolverTestHelper(obj_pair, var_pairs, con_pairs)
+  return(sth)
+}
+
+test_that("TestRelEntrQuad: expcone 1", {
+  sth <- TestRelEntrQuad.expcone_1()
+  result <- solve(sth, solver = "ECOS")
+  verify_primal_values(sth, result, tolerance = 1e-2)
+  verify_objective(sth, result, tolerance = 1e-2)
+})
+
+TestRelEntrQuad.expcone_socp_1 <- function() {
+  # A random risk-parity portfolio optimization problem.
+  sigma <- rbind(c(1.83, 1.79, 3.22),
+                 c(1.79, 2.18, 3.18),
+                 c(3.22, 3.18, 8.69))
+  L <- t(chol(sigma))
+  c <- 0.75
+  t <- Variable(name = "t")
+  x <- Variable(3, name = "x")
+  s <- Variable(3, name = "s")
+  e <- Constant(matrix(1, nrow = 3, ncol = 1))
+  
+  objective <- Minimize(t - c * t(e) %*% s)
+  con1 <- norm2(t(L) %*% x) <= t
+  con2 <- as_quad_approx(ExpCone(s, e, x), 5, 5)
+  
+  # SolverTestHelper data.
+  obj_pair <- list(objective, 4.0751197)
+  var_pairs <- list(
+    list(x, c(0.57608346, 0.54315695, 0.28037716)),
+    list(s, c(-0.55150, -0.61036, -1.27161))
+  )
+  con_pairs <- list(list(con1, 1.0),
+                    list(con2, list(NULL, NULL, NULL)))
+  sth <- SolverTestHelper(obj_pair, var_pairs, con_pairs)
+  return(sth)
+}
+
+test_that("TestRelEntrQuad: expcone socp 1", {
+  sth <- TestRelEntrQuad.expcone_socp_1()
+  result <- solve(sth, solver = "ECOS")
+  verify_primal_values(sth, result, tolerance = 1e-2)
+  verify_objective(sth, result, tolerance = 1e-2)
+})
+
+sdp_ipm_installed <- function() {
+  viable <- intersect(c("CVXOPT", "MOSEK", "COPT"), installed_solvers())
+  return(length(viable) > 0)
+}
+
+# TODO: Add TestOpRelConeQuad functions from cone2cone unit tests.
 

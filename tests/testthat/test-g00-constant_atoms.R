fde251f584445f25cc04ce157f11ad63f4d12b7d
Anqi Fu <anqif@alumni.stanford.edu>
2023-10-06 09:58:22 -0400

    WIP Update unit tests for constant atoms

--- tests/testthat/test-g00-constant_atoms.R
+++ tests/testthat/test-g00-constant_atoms.R
@@ -1,8 +1,9 @@
 context("test-constant_atoms")
 
 ROBUST_CVXOPT <- "robust_cvxopt"
+SOLVER_TO_TOL <- list(SCS = 1e-2, ECOS = 1e-7, OSQP = 1e-1)
+# SOLVER_TO_TOL <- list(SCS = 1e-2, ECOS = 5e-7, OSQP = 1e-1)   # ECOS = 1e-7 fails a few tests.
 SOLVERS_TO_TRY <- c("ECOS", "SCS", "OSQP")
-SOLVERS_TO_TOL <- list(ECOS = 5e-7, SCS = 1e-2, OSQP = 1e-1) ## ECOS = 1e-7 fails one or two tests!
 
 # Test CVXOPT if installed.
 if("CVXOPT" %in% installed_solvers()) {
@@ -14,16 +15,18 @@ if("CVXOPT" %in% installed_solvers()) {
 # Test MOSEK if installed.
 if("MOSEK" %in% installed_solvers()) {
     SOLVERS_TO_TRY <- c(SOLVERS_TO_TRY, "MOSEK")
-    SOLVERS_TO_TOL$MOSEK <- 1e-6
+    SOLVER_TO_TOL$MOSEK <- 1e-6
 }
 
 v_np <- matrix(c(-1, 2, -2), nrow = 1, ncol = 3)
 log_sum_exp_axis_1 <- function(x) { log_sum_exp(x, axis = 1) }
 log_sum_exp_axis_2 <- function(x) { log_sum_exp(x, axis = 2, keepdims = TRUE) }
 
-atoms <- list(
-    list(
-        list(
+# Map from solver name to a list of strings for atoms that fail.
+KNOWN_SOLVER_ERRORS <- list()
+KNOWN_SOLVER_ERRORS$MOSESK <- c("xexp")
+
+atoms_minimize <- list(
             list(abs, c(2,2), list(cbind(c(-5,2), c(-3,1))), Constant(cbind(c(5,2), c(3,1)))),
             list(function(x) { cumsum_axis(x, axis=1) }, c(2,2), list(cbind(c(-5,2), c(-3,1))), Constant(cbind(c(-5,2), c(-8,3)))),
             list(function(x) { cumsum_axis(x, axis=2) }, c(2,2), list(cbind(c(-5,2), c(-3,1))), Constant(cbind(c(-5,-3), c(-3,-2)))),
@@ -39,8 +42,26 @@ atoms <- list(
             list(kl_div, c(1,1), list(exp(1), 1), Constant(1)),
             list(kl_div, c(1,1), list(exp(1), exp(1)), Constant(0)),
             list(kl_div, c(2,1), list(matrix(c(exp(1), 1)), 1), Constant(c(1,0))),
+            list(rel_entr, c(1,1), list(exp(1), 1), Constant(exp(1))),
+            list(rel_entr, c(1,1), list(exp(1), exp(1)), Constant(0)),
+            list(rel_entr, c(2,1), list(matrix(c(exp(1),1)), 1), Constant(c(exp(1),0))),
+            
+            # kronecker with variable in the right operand.
             list(function(x) { kronecker(cbind(c(1,2), c(3,4)), x) }, c(4,4), list(cbind(c(5,6), c(7,8))),
                  Constant(kronecker(cbind(c(1,2), c(3,4)), cbind(c(5,6), c(7,8))))),
+            list(function(x) { kronecker(cbind(c(1,2), c(3,4), c(5,6)), x) }, c(6,4), list(cbind(c(5,6), c(7,8))),
+                 Constant(kronecker(cbind(c(1,2), c(3,4), c(5,6)), cbind(c(5,6), c(7,8))))),
+            list(function(x) { kronecker(cbind(c(1,2), c(3,4)), x) }, c(6,4), list(cbind(c(5,6), c(7,8), c(9,10))),
+                 Constant(kronecker(cbind(c(1,2), c(3,4)), cbind(c(5,6), c(7,8), c(9,10))))),
+            
+            # kronecker with variable in the left operand.
+            list(function(x) { kronecker(x, cbind(c(1,2), c(3,4))) }, c(4,4), list(cbind(c(5,6), c(7,8))),
+                 Constant(kronecker(cbind(c(5,6), c(7,8)), cbind(c(1,2), c(3,4))))),
+            list(function(x) { kronecker(x, cbind(c(1,2), c(3,4), c(5,6))) }, c(6,4), list(cbind(c(5,6), c(7,8))),
+                 Constant(kronecker(cbind(c(5,6), c(7,8)), cbind(c(1,2), c(3,4), c(5,6))))),
+            list(function(x) { kronecker(x, cbind(c(1,2), c(3,4))) }, c(6,4), list(cbind(c(5,6), c(7,8), c(9,10))),
+                 Constant(kronecker(cbind(c(5,6), c(7,8), c(9,10)), cbind(c(1,2), c(3,4))))),
+            
             list(lambda_max, c(1,1), list(cbind(c(2,0), c(0,1))), Constant(2)),
             list(lambda_max, c(1,1), list(cbind(c(2,0,0), c(0,3,0), c(0,0,1))), Constant(3)),
             list(lambda_max, c(1,1), list(cbind(c(5,7), c(7,-3))), Constant(9.06225775)),
@@ -61,16 +82,17 @@ atoms <- list(
             list(function(x) { norm(x,"2") }, c(1,1), list(v_np), Constant(3)),
             list(function(x) { norm(x,"F") }, c(1,1), list(cbind(c(-1,2), c(3,-4))), Constant(5.47722557)),
             list(function(x) { norm1(x) }, c(1,1), list(v_np), Constant(5)),
-            list(function(x) { norm1(x) }, c(1,1), list(cbind(c(-1,2), c(3,-4))), Constant(10)),
+            list(function(x) { norm1(x) }, c(1,1), list(cbind(c(-1,2), c(3,-4))), Constant(7)),
             list(function(x) { norm_inf(x) }, c(1,1), list(v_np), Constant(2)),
-            list(function(x) { norm_inf(x) }, c(1,1), list(cbind(c(-1,2), c(3,-4))), Constant(4)),
+            list(function(x) { norm_inf(x) }, c(1,1), list(cbind(c(-1,2), c(3,-4))), Constant(6)),
             list(function(x) { norm_nuc(x) }, c(1,1), list(cbind(c(2,0), c(0,1))), Constant(3)),
             list(function(x) { norm_nuc(x) }, c(1,1), list(cbind(3:5, 6:8, 9:11)), Constant(23.173260452512931)),
             list(function(x) { norm_nuc(x) }, c(1,1), list(cbind(3:5, 6:8)), Constant(14.618376738088918)),
             list(function(x) { sum_largest(abs(x),3) }, c(1,1), list(matrix(c(1,2,3,-4,-5))), Constant(5+4+3)),
+            list(function(x) { dotsort(abs(x), matrix(c(2,1,0.1))) }, c(1,1), matrix(c(1,2,3,-4,-5)), Constant(10+4+0.3)),
             list(function(x) { mixed_norm(x,1,1) }, c(1,1), list(cbind(c(1,2), c(3,4), c(5,6))), Constant(21)),
             list(function(x) { mixed_norm(x,1,1) }, c(1,1), list(cbind(1:3, 4:6)), Constant(21)),
-            list(function(x) { mixed_norm(x,2,1) }, c(1,1), list(cbind(c(3,1), c(4,sqrt(3)))), Constant(7)),
+            # list(function(x) { mixed_norm(x,2,1) }, c(1,1), list(cbind(c(3,1), c(4,sqrt(3)))), Constant(7)),
             list(function(x) { mixed_norm(x,1,Inf) }, c(1,1), list(cbind(c(1,4), c(5,6))), Constant(10)),
 
             list(p_norm, c(1,1), list(matrix(1:3)), Constant(3.7416573867739413)),
@@ -119,14 +141,15 @@ atoms <- list(
 
             ## Advanced indexing
             list(function(x) { x[cbind(c(2,3), c(1,3))] }, c(2,1), list(cbind(3:5, 6:8, 9:11)), Constant(c(4,11))),
-            list(function(x) { x[c(2,3),] }, c(2,1), list(cbind(3:5, 6:8)), Constant(cbind(c(4,5), c(7,8)))),
-            list(function(x) { x[cbind(3:5, 6:8) %% 2 == 0] }, c(3,1), list(cbind(3:5, 6:8)), Constant(c(4,6,8))),
+            list(function(x) { x[c(2,3),] }, c(2,2), list(cbind(3:5, 6:8)), Constant(cbind(c(4,5), c(7,8)))),
+            list(function(x) { x[cbind(3:5, 6:8) %% 2 == 0] }, c(2,1), list(cbind(3:5, 6:8)), Constant(c(6,4,8))),
             list(function(x) { x[seq(3,2,-1)] }, c(2,1), list(matrix(3:5)), Constant(c(5,4))),
-            list(function(x) { x[seq(3,1,-1)] }, c(3,1), list(matrix(3:5)), Constant(c(5,4,3)))
-        ),
-        Minimize),
-    list(
-        list(
+            list(function(x) { x[seq(3,1,-1)] }, c(3,1), list(matrix(3:5)), Constant(c(5,4,3))),
+            list(function(x) { x[seq(4,2,-1)] }, c(2,1), list(matrix(3:5)), Constant(c(5,4))),
+            list(function(x) { x[seq(4,1,-1)] }, c(3,1), list(matrix(3:5)), Constant(c(5,4,3)))
+          )
+
+atoms_maximize <- list(
             list(entr, c(2,2), list(cbind(c(1,exp(1)), c(exp(2), exp(-1)))), Constant(cbind(c(0,-exp(1)), c(-2*exp(2),exp(-1))))),
             list(log_det, c(1,1), list(cbind(c(20, 8, 5, 2),
                                              c(8, 16, 2, 4),
@@ -140,7 +163,7 @@ atoms <- list(
             list(function(x) { geo_mean(x, c(0.4,0.8,4.9)) }, c(1,1), list(matrix(c(0.5,1.8,17))), Constant(10.04921378316062)),
             list(harmonic_mean, c(1,1), list(matrix(c(1,2,3))), Constant(1.6363636363636365)),
             list(harmonic_mean, c(1,1), list(matrix(c(2.5,2.5,2.5,2.5))), Constant(2.5)),
-            list(harmonic_mean, c(1,1), list(matrix(c(0,1,2))), Constant(0)),
+            list(harmonic_mean, c(1,1), list(matrix(c(1e-8,1,2))), Constant(0)),
 
             list(function(x) { diff(x, differences = 0) }, c(3,1), list(matrix(c(1,2,3))), Constant(c(1,2,3))),
             list(diff, c(2,1), list(matrix(c(1,2,3))), Constant(c(1,1))),
@@ -172,111 +195,145 @@ atoms <- list(
             list(function(x) { sum_smallest(x,3) }, c(1,1), list(matrix(c(-1,2,3,4,5))), Constant(-1+2+3)),
             list(function(x) { sum_smallest(x,4) }, c(1,1), list(cbind(c(-3,-4,5), c(6,7,8), c(9,10,11))), Constant(-3-4+5+6)),
             list(function(x) { (x + Constant(0))^0.5 }, c(2,2), list(cbind(c(2,4), c(16,1))), Constant(cbind(c(1.414213562373095,2), c(4,1))))
-        ),
-        Maximize
-    )
-)
+          )
 
+# Can the solver solver the problem?
 check_solver <- function(prob, solver_name) {
-    tryCatch({
-        if(solver_name == ROBUST_CVXOPT)
-            solver_name <- "CVXOPT"
-
-        chains <- CVXR:::.construct_chains(prob, solver = solver_name)
-        return(TRUE)
-    }, error = function(e) {
+  atom_str <- as.character(prob@objective@args[[1]])
+  if(solver_name %in% names(KNOWN_SOLVER_ERRORS)) {
+    for(bad_atom_name in KNOWN_SOLVER_ERRORS[[solver_name]]) {
+      if(grepl(bad_atom_name, atom_str, fixed = TRUE))
         return(FALSE)
-    })
+    }
+  }
+  
+  tryCatch({
+      if(solver_name == ROBUST_CVXOPT)
+          solver_name <- "CVXOPT"
+
+      chains <- CVXR:::.construct_chains(prob, solver = solver_name)
+      
+      return(TRUE)
+  }, error = function(e) {
+      return(FALSE)
+  })
 }
 
-ecnt  <- 0
+ecnt  <- 0   # Count number of tests.
+
+# Test numeric version of atoms.
 run_atom <- function(atom, problem, obj_val, solver, verbose = FALSE) {
-    expect_true(is_dcp(problem))
-    ##print(problem)
-    if(verbose) {
-        print(problem@objective)
-        print(problem@constraints)
-        print(paste("solver", solver))
-    }
+  expect_true(is_dcp(problem))
+  print(problem)
+  
+  if(verbose) {
+    print(problem@objective)
+    print(problem@constraints)
+    print(paste("solver", solver))
+  }
 
-    if(check_solver(problem, solver)) {
-        tolerance <- SOLVERS_TO_TOL[[solver]]
+  if(check_solver(problem, solver)) {
+    tolerance <- SOLVERS_TO_TOL[[solver]]
+    
+    tryCatch({
+      if(solver == ROBUST_CVXOPT)
+        result <- solve(problem, solver = CVXOPT, verbose = verbose, kktsolver = ROBUST_KKTSOLVER)
+      else
         result <- solve(problem, solver = solver, verbose = verbose)
-
-        if(result$status %in% c("optimal", "optimal_inaccurate")) {
-            if(verbose) {
-                print(result$value)
-                print(obj_val)
-            }
-
-            obj_diff <- (result$value - obj_val)/(1+abs(obj_val))
-            expect_true(abs(obj_diff) <= tolerance)
-
-            if(abs(obj_diff) > tolerance) {
-                ecnt  <- ecnt + 1
-                sink("test_constant_atoms_out.txt", append = TRUE)
-                cat(sprintf("Solver: %s\n", solver))
-                print(atom)
-                cat(sprintf("Result: %.10f \t Expected: %.10f \t Obj_diff: %.10f\n", result$value, obj_val, obj_diff))
-                sink()
-                saveRDS(list(problem  = problem, solver = "solver"), file = sprintf("error-%02d.RDS", ecnt))
-            }
-        } else
-            stop("Problem status is sub-optimal: ", result$status)
-    }
+    }, error = function(e) {
+      if(solver %in% names(KNOWN_SOLVER_ERRORS) && atom %in% KNOWN_SOLVER_ERRORS[[solver]])
+        return()
+      stop(e)
+    })
+    
+    if(result$status %in% SOLUTION_PRESENT) {
+      if(verbose) {
+        print(result$value)
+        print(obj_val)
+      }
+      
+      obj_diff <- (result$value - obj_val)/(1 + abs(obj_val))
+      expect_true(abs(obj_diff) <= tolerance)
+      
+      if(abs(obj_diff) > tolerance) {
+        ecnt  <- ecnt + 1
+        sink("test_constant_atoms_out.txt", append = TRUE)
+        cat(sprintf("Solver: %s\n", solver))
+        print(atom)
+        cat(sprintf("Result: %.10f \t Expected: %.10f \t Obj_diff: %.10f\n", result$value, obj_val, obj_diff))
+        sink()
+        saveRDS(list(problem  = problem, solver = "solver"), file = sprintf("error-%02d.RDS", ecnt))
+      }
+    } else
+        stop("Problem status is sub-optimal: ", result$status)
+  }
 }
+
+get_indices <- function(size) {
+  # Get indices for dimension.
+  if(length(size) == 0)
+    return(c(1))
+  else if(length(size) == 1)
+    return(seq(size[1]))
+  else
+    expand.grid(seq(size[1]), seq(size[2]))
+}
+
+atoms_minimize <- lapply(atoms_minimize, function(a) { list(a, Minimize) })
+atoms_maximize <- lapply(atoms_maximize, function(a) { list(a, Maximize) })
 
 test_that("Test all constant atoms", {
-    skip_on_cran()
-    ## if(file.exists("test_constant_atoms_out.txt"))
-    ##  file.remove("test_constant_atoms_out.txt")
-    ## atom_part  <- 0 ## counter for atom part (DEBUG AID)
-    for(a in atoms) {
-        atom_list <- a[[1]]
-        objective_type <- a[[2]]
-        ## counter  <- 0 ## list item counter (DEBUG AID)
-        for(al in atom_list) {
-            ##counter  <- counter + 1 ## list item counter (DEBUG AID)
-            atom <- al[[1]]
-            dims <- al[[2]]
-            args <- al[[3]]
-            obj_val <- al[[4]]
-            for(row in 1:dims[1]) {
-                for(col in 1:dims[2]) {
-                    for(solver in SOLVERS_TO_TRY) {
-                        ## cat(sprintf("i: %d j: %d solver: %S \n", atom_part, counter, solver)) ## (DEBUG AID)
-                        ## Atoms with Constant arguments
-                        const_args <- lapply(args, Constant)
-                        run_atom(atom, Problem(objective_type(do.call(atom, const_args)[row, col])),
-                                 value(obj_val[row, col]), solver)
+  skip_on_cran()
+  ## if(file.exists("test_constant_atoms_out.txt"))
+  ##  file.remove("test_constant_atoms_out.txt")
+  ## atom_part  <- 0 ## counter for atom part (DEBUG AID)
+  for(a in atoms) {
+      atom_list <- a[[1]]
+      objective_type <- a[[2]]
+      ## counter  <- 0 ## list item counter (DEBUG AID)
+      for(al in atom_list) {
+          ##counter  <- counter + 1 ## list item counter (DEBUG AID)
+          atom <- al[[1]]
+          dims <- al[[2]]
+          args <- al[[3]]
+          obj_val <- al[[4]]
+          for(row in 1:dims[1]) {
+              for(col in 1:dims[2]) {
+                  for(solver in SOLVERS_TO_TRY) {
+                      ## cat(sprintf("i: %d j: %d solver: %S \n", atom_part, counter, solver)) ## (DEBUG AID)
+                      ## Atoms with Constant arguments
+                      const_args <- lapply(args, Constant)
+                      run_atom(atom, Problem(objective_type(do.call(atom, const_args)[row, col])),
+                               value(obj_val[row, col]), solver)
 
-                        ## Atoms with Variable arguments
-                        variables <- list()
-                        constraints <- list()
-                        for(expr in args) {
-                            expr_dim <- CVXR:::intf_dim(expr)
-                            variables <- c(variables, Variable(expr_dim[1], expr_dim[2]))
-                            constraints <- c(constraints, variables[[length(variables)]] == expr)
-                        }
-                        objective <- objective_type(do.call(atom, variables)[row, col])
-                        ## print(atom)
-                        ## print(value(obj_val[row, col]))
-                        run_atom(atom, Problem(objective, constraints), value(obj_val[row, col]), solver)
+                      ## Atoms with Variable arguments
+                      variables <- list()
+                      constraints <- list()
+                      for(expr in args) {
+                          expr_dim <- CVXR:::intf_dim(expr)
+                          # variables <- c(variables, Variable(expr_dim[1], expr_dim[2]))
+                          variables <- c(variables, new("Variable", dim = expr_dim))
+                          constraints <- c(constraints, variables[[length(variables)]] == expr)
+                      }
+                      objective <- objective_type(do.call(atom, variables)[row, col])
+                      ## print(atom)
+                      ## print(value(obj_val[row, col]))
+                      run_atom(atom, Problem(objective, constraints), value(obj_val[row, col]), solver)
 
-                        ## Atoms with Parameter arguments
-                        ## parameters <- list()
-                        ## for(expr in args) {
-                        ##     expr_dim <- dim(expr)
-                        ##     parameters <- c(parameters, Parameter(expr_dim[1], expr_dim[2]))
-                        ##     value(parameters[[length(parameters)]]) <- as.matrix(expr)
-                        ## }
-                        ## objective <- objective_type(do.call(atom, parameters)[row, col])
-                        ## run_atom(atom, Problem(objective), value(obj_val[row, col]), solver)
-                    }
-                }
-            }
-        }
-    }
+                      ## Atoms with Parameter arguments
+                      ## parameters <- list()
+                      ## for(expr in args) {
+                      ##     expr_dim <- dim(expr)
+                      ##     parameters <- c(parameters, Parameter(expr_dim[1], expr_dim[2]))
+                      ##     value(parameters[[length(parameters)]]) <- as.matrix(expr)
+                      ## }
+                      ## objective <- objective_type(do.call(atom, parameters)[row, col])
+                      ## run_atom(atom, Problem(objective), value(obj_val[row, col]), solver)
+                  }
+              }
+          }
+      }
+  }
 })
 
 ## To isolate a problem with a particular atom, run the test below
